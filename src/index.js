0edfea6cf31021f6203d1238fb070e466db0770b
Eryn <eryn@eryn.io>
2017-04-12 22:29:26 -0400

    Update client.json documentation

--- src/index.js
+++ src/index.js
@@ -23,14 +23,18 @@ if (config.updateServer) {
 
 // client.json documentation:
 /*
-    "token"         : String. The bot token that is used to log in to your bot.
-    "lockNicknames" : Boolean. Default false. If true, the bot will run DiscordServer.verifyMember every time
-                      they begin typing. This will quickly eat up API requests if you aren't careful. Mostly
-                      used on the hosted version.
-    "updateServer"  : {
-                      If this object is present, the update server will be started.
+    "token"             : String. The bot token that is used to log in to your bot.
+    "lockNicknames"     : Boolean. Default false. If true, the bot will run DiscordServer.verifyMember every time
+                          they begin typing. This will quickly eat up API requests if you aren't careful. Mostly
+                          used on the hosted version.
+    "updateServer"      : {
+                          If this object is present, the update server will be started.
                       
-        "port"      : The port the Update server runs on.
-        "apiKey"    : The API key the server checks against before updating the user.
+        "port"          : Integer. The port the Update server runs on.
+        "apiKey"        : String. The API key the server checks against before updating the user.
     }
+    "loud"              : Boolean. Default false. Logs every request made to stdout.
+    "totalShards"       : Integer. Default auto. The number of shards to launch.
+    "apiRequestMethod"  : String. Default 'sequential'. sequential' or 'burst'. Sequential executes all requests in the order 
+                          they are triggered, whereas burst runs multiple at a time, and doesn't guarantee a particular order.
 */

feaa0f9740e3c40605a2ab50f7cf6193ba769ca9
Eryn Lynn <eryn@eryn.io>
2017-04-19 17:06:47 -0400

    Return after errors in update server

--- src/UpdateServer.js
+++ src/UpdateServer.js
@@ -10,11 +10,11 @@ function(shardingManager, config) {
 
     server.get('/update-user', (req, res) => {
         if (config.apiKey && config.apiKey !== req.query.apiKey) {
-            res.status(403).end();
+            return res.status(403).end();
         }
 
         if (req.query.id == null) {
-            res.status(400).end();
+            return res.status(400).end();
         }
 
         let id = req.query.id;

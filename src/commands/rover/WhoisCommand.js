fb0fc847f20572eb4c0d2b375edda2f77e480e23
Eryn Lynn <eryn@eryn.io>
2017-05-29 12:32:23 -0400

    Tabs to spaces

--- src/commands/rover/WhoisCommand.js
+++ src/commands/rover/WhoisCommand.js
@@ -17,35 +17,35 @@ class WhoisCommand extends Command {
                     label: 'member',
                     prompt: "What is their username?",
                     type: 'member',
-					default: false,
+                    default: false,
                 }
             ]
         });
     }
 
     async fn(msg, args) {
-	let member = args.member;
-	let data = {};
-	let id = member.user.id;
-	if (member) { // If the member specified exists,
-		try {
-			// Read user data from memory, or request it if there isn't any cached.
-			data = DiscordServer.DataCache[member.user.id] || await request({
-				uri: `https://verify.eryn.io/api/user/${id}`,
-				json: true,
-				simple: false
-			})
-		} catch (e) {
-			console.log(e);
-			return msg.reply("An error occured while fetching that user's data.")
-		}
-		if (data.status === "ok"){
-			// Make sure the data is cached so we don't have to use the API in the future
-			DiscordServer.DataCache[id] = data;
-			msg.reply(`${member.displayName}: https://www.roblox.com/users/${data.robloxId}/profile`);
-		} else {
-			msg.reply(`${member.displayName} doesn't seem to be verified.`)
-		}
-	}
+    let member = args.member;
+    let data = {};
+    let id = member.user.id;
+    if (member) { // If the member specified exists,
+        try {
+            // Read user data from memory, or request it if there isn't any cached.
+            data = DiscordServer.DataCache[member.user.id] || await request({
+                uri: `https://verify.eryn.io/api/user/${id}`,
+                json: true,
+                simple: false
+            })
+        } catch (e) {
+            console.log(e);
+            return msg.reply("An error occured while fetching that user's data.")
+        }
+        if (data.status === "ok"){
+            // Make sure the data is cached so we don't have to use the API in the future
+            DiscordServer.DataCache[id] = data;
+            msg.reply(`${member.displayName}: https://www.roblox.com/users/${data.robloxId}/profile`);
+        } else {
+            msg.reply(`${member.displayName} doesn't seem to be verified.`)
+        }
+    }
     }
 }

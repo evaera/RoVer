b20a609470629ff3bc7c42bc848638bde749be36
Eryn Lynn <eryn@eryn.io>
2018-01-13 18:24:58 -0500

    Add VerifyChannel, Fix #78

---
+++ src/commands/rover/VerifyChannelCommand.js
@@ -0,0 +1,34 @@
+const Command = require('../Command')
+
+module.exports =
+class VerifyChannelCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'verifychannel',
+      aliases: ['roververifychannel'],
+      description: '`<Discord Channel>` Set a channel that the bot will delete all messages in except for verification messages. Default none.',
+
+      args: [
+        {
+          key: 'channel',
+          label: 'channel',
+          prompt: 'What channel will users run the verify command in?',
+          type: 'channel',
+          default: false
+        }
+      ]
+    })
+  }
+
+  async fn (msg, args) {
+    let channel = args.channel
+
+    if (channel) {
+      this.server.setSetting('verifyChannel', channel.id)
+      msg.reply(`Set verify channel to ${channel}. Non-verification messages in this channel will be deleted.`)
+    } else {
+      this.server.setSetting('verifyChannel', null)
+      msg.reply('Removed verification channel. All messages are allowed to be sent in that channel now.')
+    }
+  }
+}

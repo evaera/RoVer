8001c4b84b8ed423dace319dabda5d933a592e34
Eryn Lynn <eryn@eryn.io>
2018-01-13 18:00:58 -0500

    Add joindm command, fix #82

---
+++ src/commands/rover/JoinMessageCommand.js
@@ -0,0 +1,30 @@
+const Command = require('../Command')
+
+module.exports =
+class JoinMessageCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'joindm',
+      aliases: ['roverjoindm'],
+      description: '`<on|off>` Set whether or not new users will be automatically direct messaged when joining this server. Default on',
+
+      args: [
+        {
+          key: 'state',
+          label: 'state',
+          prompt: 'Should users get direct messaged with your welcome message or verification instructions when they join this server? (on|off)',
+          type: 'boolean'
+        }
+      ]
+    })
+  }
+
+  async fn (msg, args) {
+    this.server.setSetting('joinDM', args.state)
+    if (args.state) {
+      msg.reply('The bot will now direct message new users with the welcome message or verification instructions.')
+    } else {
+      msg.reply('The bot will no longer direct message new users with the welcome message or verification instructions.')
+    }
+  }
+}

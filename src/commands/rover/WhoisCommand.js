3cd5703439fb1ecce1cd42ee4f7b3f5440da2035
Eryn Lynn <eryn@eryn.io>
2018-01-14 14:13:31 -0500

    Change order of match

--- src/commands/rover/WhoisCommand.js
+++ src/commands/rover/WhoisCommand.js
@@ -75,8 +75,8 @@ class WhoisCommand extends Command {
           })
 
           joinDate = profileSource.match(/Join Date<p class=text-lead>(.*?)<li/)[1]
-          pastNames = profileSource.match(/<span class=tooltip-pastnames data-toggle=tooltip title="?(.*?)"?>/)[1]
           bio = profileSource.match(/<meta name=description content=".*? is one of millions playing, creating and exploring the endless possibilities of Roblox. Join .*? on Roblox and explore together! ((?:.|\n))*?"/m)[1]
+          pastNames = profileSource.match(/<span class=tooltip-pastnames data-toggle=tooltip title="?(.*?)"?>/)[1]
         } catch (e) {}
 
         let bc = 'Unknown'

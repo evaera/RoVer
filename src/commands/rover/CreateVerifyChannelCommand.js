5ec4b5838d40bf4561dc76372faa23b7833bb48e
Gabe <1skydran@gmail.com>
2018-03-14 18:38:49 -0400

    Wrap CreateVerifyChannel in try catch (#114) closes #104
    * Add try/catch
    
    * update message to send proper reply with no permission
    
    * Standard Javascript Format

--- src/commands/rover/CreateVerifyChannelCommand.js
+++ src/commands/rover/CreateVerifyChannelCommand.js
@@ -13,46 +13,50 @@ class CreateVerifyChannelCommand extends Command {
   }
 
   async fn (msg) {
-    let category = await msg.guild.channels.create('Verification', {
-      type: 'category',
-      reason: `${msg.member.displayName} ran CreateVerifyChannel command`
-    })
-
-    let overwritesInstructions = []
-    let overwritesVerify = []
-
-    for (let role of msg.guild.roles.array()) {
-      overwritesInstructions.push({
-        id: role,
-        allow: ['READ_MESSAGE_HISTORY', 'VIEW_CHANNEL'],
-        deny: ['SEND_MESSAGES', 'ADD_REACTIONS']
+    try {
+      let category = await msg.guild.channels.create('Verification', {
+        type: 'category',
+        reason: `${msg.member.displayName} ran CreateVerifyChannel command`
       })
 
-      overwritesVerify.push({
-        id: role,
-        allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-        deny: ['READ_MESSAGE_HISTORY']
+      let overwritesInstructions = []
+      let overwritesVerify = []
+
+      for (let role of msg.guild.roles.array()) {
+        overwritesInstructions.push({
+          id: role,
+          allow: ['READ_MESSAGE_HISTORY', 'VIEW_CHANNEL'],
+          deny: ['SEND_MESSAGES', 'ADD_REACTIONS']
+        })
+
+        overwritesVerify.push({
+          id: role,
+          allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+          deny: ['READ_MESSAGE_HISTORY']
+        })
+      }
+
+      let instructionsChannel = await msg.guild.channels.create('verify-instructions', {
+        type: 'text',
+        parent: category,
+        reason: `${msg.member.displayName} ran CreateVerifyChannel command`,
+        overwrites: overwritesInstructions
       })
+
+      let verifyChannel = await msg.guild.channels.create('verify', {
+        type: 'text',
+        parent: category,
+        reason: `${msg.member.displayName} ran CreateVerifyChannel command`,
+        overwrites: overwritesVerify
+      })
+
+      this.server.setSetting('verifyChannel', verifyChannel.id)
+
+      instructionsChannel.send(`This server uses a Roblox verification system. In order to unlock all the features of this server, you'll need to verify your Roblox account with your Discord account!\n\nVisit ${Util.getVerifyLink(msg.guild)} and follow the instructions. Then, say \`${msg.guild.commandPrefix}verify\` in ${verifyChannel} and it will update you accordingly.`)
+
+      msg.reply(`Created channels ${verifyChannel} and ${instructionsChannel}. You can delete the default message in the instructions channel and replace it with your own if you wish.`)
+    } catch (err) {
+      msg.reply(`:no_entry_sign: RoVer does not have permission to create channels in this server.`)
     }
-
-    let instructionsChannel = await msg.guild.channels.create('verify-instructions', {
-      type: 'text',
-      parent: category,
-      reason: `${msg.member.displayName} ran CreateVerifyChannel command`,
-      overwrites: overwritesInstructions
-    })
-
-    let verifyChannel = await msg.guild.channels.create('verify', {
-      type: 'text',
-      parent: category,
-      reason: `${msg.member.displayName} ran CreateVerifyChannel command`,
-      overwrites: overwritesVerify
-    })
-
-    this.server.setSetting('verifyChannel', verifyChannel.id)
-
-    instructionsChannel.send(`This server uses a Roblox verification system. In order to unlock all the features of this server, you'll need to verify your Roblox account with your Discord account!\n\nVisit ${Util.getVerifyLink(msg.guild)} and follow the instructions. Then, say \`${msg.guild.commandPrefix}verify\` in ${verifyChannel} and it will update you accordingly.`)
-
-    msg.reply(`Created channels ${verifyChannel} and ${instructionsChannel}. You can delete the default message in the instructions channel and replace it with your own if you wish.`)
   }
 }

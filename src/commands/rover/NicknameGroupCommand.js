73d08227f683c9bec83d285be265a2dcad7df7b6
slixxed <1skydran@gmail.com>
2018-03-14 03:52:10 -0400

    requested changes

--- src/commands/rover/NicknameGroupCommand.js
+++ src/commands/rover/NicknameGroupCommand.js
@@ -15,19 +15,27 @@ class NicknameGroupCommand extends Command {
           label: 'groupid',
           prompt: 'Nickname group',
           type: 'string',
-          infinite: true
+          infinite: false
         }
       ]
     })
   }
 
   async fn (msg, args) {
-    if (args.groupid) {
-      this.server.setSetting('nicknameGroup', args.groupid)
-      msg.reply(`Set nickname group to \`${args.groupid}\``)
+    let groupId = args.groupid
+    if (groupId) {
+      if (groupId.match(/^\d+$/)) {
+        this.server.setSetting('nicknameGroup', args.groupid)
+        msg.reply(`Set nickname group to \`${args.groupid}\``)
+      } else {
+        console.log('NOPE')
+        msg.reply(`:no_entry_sign: You have attempted to bind an invalid group. Possible causes:\n\n- You forgot to put the Discord role name in quotation marks when it has spaces\n- You have attempted to bind an invalid group id. Group IDs must be a whole number or be a valid VirtualGroup name.`)
+      }
+      // this.server.setSetting('nicknameGroup', args.groupid)
+      // msg.reply(`Set nickname group to \`${args.groupid}\``)
     } else {
       this.server.setSetting('nicknameGroup', undefined)
-      msg.reply('Nickname group has been set to null.')
+      msg.reply('Nickname group has been removed.')
     }
   }
 }

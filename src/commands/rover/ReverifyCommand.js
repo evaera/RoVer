6d229eaef73d049f4b8958e1acb3626126e1d0ff
Eryn Lynn <eryn@eryn.io>
2018-03-25 09:57:49 -0400

    ReverifyCommand

---
+++ src/commands/rover/ReverifyCommand.js
@@ -0,0 +1,18 @@
+const Util = require('../../Util')
+const Command = require('../Command')
+
+module.exports =
+class ReverifyCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'reverify',
+      properName: 'Reverify',
+      aliases: ['roverreverify'],
+      description: 'Displays instructions on how to reverify'
+    })
+  }
+
+  async fn (msg) {
+    msg.reply(`To change your verified account, visit ${Util.getVerifyLink(msg.guild)} and verify again.`)
+  }
+}

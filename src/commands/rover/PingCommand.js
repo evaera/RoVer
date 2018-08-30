19331ca80a2ce150b8d17a3d4c75a599b3e2389c
Will <35583237+whasonyt@users.noreply.github.com>
2018-08-30 19:17:39 -0400

    Update PingCommand.js
    fixed what u wanted

--- src/commands/rover/PingCommand.js
+++ src/commands/rover/PingCommand.js
@@ -1,22 +1,19 @@
 const Command = require('../Command')
 
 module.exports =
-class HelpCommand extends Command {
+class PingCommand extends Command {
   constructor (client) {
     super(client, {
-      name: 'ping',
+      name: 'Ping',
       properName: 'RoVer',
       description: 'Ping the bot to see API latency'
     })
   }
-
   async fn (msg) {
-   
-    const start = Date.now()
-    message.channel.send("Pinging...").then(message => {
-        const end = Date.now()
-        message.edit(`:ping_pong: Pong! Took **${(end - start)}**ms`);
+      const start = Date.now()
+      message.channel.send("Pinging...").then(message => {
+      const end = Date.now()
+      message.edit(`:ping_pong: Pong! Took **${(end - start)}**ms`);
     })
-
   }
 }

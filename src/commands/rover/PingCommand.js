604044851ba5f6478a5f0237eec06ee57bbb50f6
Eryn Lynn <eryn@eryn.io>
2018-08-31 21:17:36 -0400

    Allow ping command to be used by anyone

--- src/commands/rover/PingCommand.js
+++ src/commands/rover/PingCommand.js
@@ -6,7 +6,8 @@ class PingCommand extends Command {
     super(client, {
       name: 'ping',
       properName: 'Ping',
-      description: 'Ping the bot to see API latency'
+      description: 'Ping the bot to see API latency',
+      userPermissions: []
     })
   }
   async fn (msg) {

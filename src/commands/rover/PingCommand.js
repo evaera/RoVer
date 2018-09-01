6afd4aedb40f27414dab05c7a918b98687a50b56
Eryn Lynn <eryn@eryn.io>
2018-08-31 21:08:03 -0400

    Fix ping command

--- src/commands/rover/PingCommand.js
+++ src/commands/rover/PingCommand.js
@@ -4,16 +4,14 @@ module.exports =
 class PingCommand extends Command {
   constructor (client) {
     super(client, {
-      name: 'Ping',
-      properName: 'RoVer',
+      name: 'ping',
+      properName: 'Ping',
       description: 'Ping the bot to see API latency'
     })
   }
   async fn (msg) {
-    const start = Date.now()
     msg.channel.send('Pinging...').then(message => {
-      const end = Date.now()
-      message.edit(`:ping_pong: Pong! Took **${(end - start)}**ms`)
+      message.edit(`:ping_pong: Pong! Took **${message.createdTimestamp - msg.createdTimestamp}ms**`)
     })
   }
 }

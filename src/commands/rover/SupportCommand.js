4ef2c186c2c14fa9e947c3e71df0962e7ac51757
PhoenixShay <PhoenixShay@users.noreply.github.com>
2018-10-15 18:48:16 -0400

    command can't be used in non-server channels
    oops

--- src/commands/rover/SupportCommand.js
+++ src/commands/rover/SupportCommand.js
@@ -15,9 +15,7 @@ class AnnounceChannelCommand extends Command {
   async fn (msg) {
     try {
       msg.author.send('Having trouble? You can join our official support discord here: discord.gg/7yfwrat, or you can check out the documentation here: https://rover.link/#readme')
-      if (msg.member) { // Wouldn't want to say this if it's in a DM conversation
-        msg.reply('Sent you a DM with information.')
-      }
+      msg.reply('Sent you a DM with information.')
     } catch (e) {
       msg.reply("I can't seem to message you - please make sure your DMs are enabled!")
     }

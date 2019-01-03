eff5d79f40135da6386d5cc594aa9765865cd887
Beta <21072460+bvta@users.noreply.github.com>
2019-01-03 00:23:52 -0600

    removed .then in an async function

--- src/commands/rover/PingCommand.js
+++ src/commands/rover/PingCommand.js
@@ -1,18 +1,17 @@
-const Command = require('../Command')
+const Command = require('../Command');
 
 module.exports =
-class PingCommand extends Command {
-  constructor (client) {
-    super(client, {
-      name: 'ping',
-      properName: 'Ping',
-      description: 'Ping the bot to see API latency',
-      userPermissions: []
-    })
-  }
-  async fn (msg) {
-    msg.channel.send('Pinging...').then(message => {
-      message.edit(`:ping_pong: Pong! Took **${message.createdTimestamp - msg.createdTimestamp}ms**`)
-    })
-  }
-}
+   class PingCommand extends Command {
+      constructor(client) {
+         super(client, {
+            name: 'ping',
+            properName: 'Ping',
+            description: 'Ping the bot to see API latency',
+            userPermissions: []
+         })
+      }
+      async fn(msg) {
+         const m = msg.channel.send('Pinging...');
+         await m.edit(`:ping_pong: Pong! Took **${message.createdTimestamp - msg.createdTimestamp}ms**`);
+      }
+   }

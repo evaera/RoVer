0524ac9910bc8cf5128ca4dd94c0a4cda34d912f
Eryn Lynn <eryn@eryn.io>
2020-04-01 15:57:16 -0400

    update unauthorized error message

--- src/DiscordBot.js
+++ src/DiscordBot.js
@@ -67,7 +67,11 @@ class DiscordBot {
     if (this.isPremium()) {
       this.bot.dispatcher.addInhibitor(msg => {
         if (msg.guild && !this.authorizedOwners.includes(msg.guild.ownerID)) {
-          msg.reply(`Sorry, this server isn't authorized to use RoVer Plus.${msg.member.hasPermission(['MANAGE_GUILD']) ? ' The server owner needs to donate at <https://www.patreon.com/erynlynn>, or you can invite the regular RoVer bot at <https://RoVer.link>.' : ''}`) // notify sender to donate only if they're an "admin"
+          if (this.authorizedOwners.length === 0) {
+            msg.reply("Sorry, the authorized users list is still being downloaded. This occurs when the bot has recently restarted. Please wait a few seconds and try again.")
+          } else {
+            msg.reply(`Sorry, this server isn't authorized to use RoVer Plus.${msg.member.hasPermission(['MANAGE_GUILD']) ? ' The server owner needs to donate at <https://www.patreon.com/erynlynn>, or you can invite the regular RoVer bot at <https://RoVer.link>.' : ''}`) // notify sender to donate only if they're an "admin"
+          }
 
           return 'not_premium'
         }

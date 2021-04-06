7bda8ff6f14beb961d3e05e40658ccceb6160338
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-04-06 13:15:26 -0400

    Unverify command (#330)
    * Add unverify command
    
    * Forgot invite link

---
+++ src/commands/rover/UnverifyCommand.js
@@ -0,0 +1,21 @@
+const Command = require('../Command')
+
+module.exports =
+class UnverifyCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'unverify',
+      properName: 'Unverify',
+      aliases: ['unlink'],
+      userPermissions: []
+    })
+  }
+
+  async fn (msg) {
+    msg.author.send('To unverify, please contact a moderator or above at our support server: https://discord.gg/7yfwrat').then(() => {
+      msg.reply('Sent you a DM with information.')
+    }).catch(() => {
+      msg.reply('I can\'t seem to message you - please make sure your DMs are enabled!')
+    })
+  }
+}

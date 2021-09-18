b84a07fb7436c7e5e33d138c49263ec5eae036b3
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-17 23:32:07 -0400

    Set empty string as arg default
    When setting the default value to false, parseArgs() returns a stringified 'false' which would prevent self-updating by omitting the argument.

--- src/commands/rover/UpdateCommand.js
+++ src/commands/rover/UpdateCommand.js
@@ -55,7 +55,7 @@ class UpdateCommand extends Command {
           key: 'user',
           prompt: 'User to update',
           type: 'user|role',
-          default: false
+          default: ''
         }
       ]
     })

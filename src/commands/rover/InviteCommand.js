c115e32fc80fb4e7afe0e0250496d49c0744299a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-22 09:22:56 -0400

    Send patreon post link if bot is premium

--- src/commands/rover/InviteCommand.js
+++ src/commands/rover/InviteCommand.js
@@ -14,7 +14,7 @@ class InviteCommand extends Command {
   }
 
   async fn (msg) {
-    msg.author.send(`Use the following link to invite RoVer: <${config.invite}>`).then(() => {
+    msg.author.send(this.discordBot.isPremium() ? 'Visit https://www.patreon.com/posts/rover-plus-28480174 to invite RoVer Plus' : `Use the following link to invite RoVer: <${config.invite}>`).then(() => {
       msg.reply('Sent you a DM with information.')
     }).catch(() => {
       msg.reply('I can\'t seem to message you - please make sure your DMs are enabled!')

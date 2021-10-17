5b069a1d7cd7e3ed66aa4fb9b6be8e5cec5c58f3
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-17 01:44:35 -0400

    Reformat with prettier

--- src/commands/rover/InviteCommand.js
+++ src/commands/rover/InviteCommand.js
@@ -1,23 +1,35 @@
-const Command = require('../Command')
-const config = require('../../data/client.json')
+const Command = require("../Command");
+const config = require("../../data/client.json");
 
-module.exports =
-class InviteCommand extends Command {
-  constructor (client) {
+module.exports = class InviteCommand extends Command {
+  constructor(client) {
     super(client, {
-      name: 'invite',
-      properName: 'Invite',
-      aliases: ['roverinvite'],
-      description: 'Sends the user an invite link to invite RoVer.',
-      userPermissions: []
-    })
+      name: "invite",
+      properName: "Invite",
+      aliases: ["roverinvite"],
+      description: "Sends the user an invite link to invite RoVer.",
+      userPermissions: [],
+    });
   }
 
-  async fn (msg) {
-    msg.author.send(this.discordBot.isPremium() ? 'Visit https://www.patreon.com/posts/rover-plus-28480174 to invite RoVer Plus' : `${config.invite ? `Use the following link to invite RoVer: <${config.invite}>` : 'Visit https://rover.link to invite RoVer'}`).then(() => {
-      msg.reply('Sent you a DM with information.')
-    }).catch(() => {
-      msg.reply('I can\'t seem to message you - please make sure your DMs are enabled!')
-    })
+  async fn(msg) {
+    msg.author
+      .send(
+        this.discordBot.isPremium()
+          ? "Visit https://www.patreon.com/posts/rover-plus-28480174 to invite RoVer Plus"
+          : `${
+              config.invite
+                ? `Use the following link to invite RoVer: <${config.invite}>`
+                : "Visit https://rover.link to invite RoVer"
+            }`
+      )
+      .then(() => {
+        msg.reply("Sent you a DM with information.");
+      })
+      .catch(() => {
+        msg.reply(
+          "I can't seem to message you - please make sure your DMs are enabled!"
+        );
+      });
   }
-}
+};
